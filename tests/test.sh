d2ccb092540b4935c8d6f2f94ddf87c994453ded
Satoru SATOH <satoru.satoh@gmail.com>
2018-11-03 22:27:08 +0900

    change: make test cases run twice w/ different condition at once

--- tests/test.sh
+++ tests/test.sh
@@ -5,5 +5,4 @@ cd ${0%/*}/../  # cd to the topdir.
 yamllint --strict $(git ls-files '*.yaml' '*.yml')
 which ansible-lint 2>/dev/null && ansible-lint -vvv tests/test.yml || ansible-lint-3 -vvv tests/test.yml
 ansible-playbook -vvv tests/test.yml -i tests/inventory --syntax-check
-ansible-playbook -vvv tests/test.yml -i tests/inventory --extra-vars openssl_cert_generate_self_signed_cert=true
-
+ansible-playbook -vvv tests/test.yml -i tests/inventory
